552fc844c8ed468f246c7ebd199590622f148a0d
Mikael Lindqvist <li.mikael@gmail.com>
2020-02-11 18:08:05 +0800

    testing css

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -9,13 +9,26 @@ export default class App {
 
 	render() {
 		return (
-			<div>
-				<a href="#" onClick={this.onAddClick} class="add-button">add</a>
+			<div class="app">
+				<div>hello some kind of long text goes here... does it wrap ok?</div>
+				<div style={{position: 'absolute'}}>
+					<a href="#" onClick={this.onAddClick} class="my-add-link" style={{top: 0, position: 'absolute'}}>adify</a>
+					<div style={{display: 'block'}} style={{top: 0, position: 'absolute', left: '5em'}}>
+						<div class="ln1"/><div class="ln2"/>
+						<div class="ln1"/>
+					</div>
+				</div>
 				<ul>
 					{this.context.items.map((item)=>{
 						return <li>{item}</li>
 					})}
 				</ul>
+				<div>
+					<div class="test1"/><div class="test2"/>
+				</div>
+				<div class="foot">
+					hello i'm the footer...
+				</div>
 			</div>
 		);
 	}
