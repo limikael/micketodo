edfe11325b299f9b5ebe8f90c7a2f8b45af33249
Mikael Lindqvist <li.mikael@gmail.com>
2020-02-11 18:19:54 +0800

    dir structure

--- hooks/bundle.sh
+++ hooks/bundle.sh
@@ -1,3 +1,3 @@
 echo "Bundling javascript"
-./node_modules/.bin/browserify -t babelify --debug src/index.jsx -o www/js/index.js
+./node_modules/.bin/browserify -t babelify --debug src/index.jsx -o www/index.js
 #./node_modules/.bin/browserify -t babelify -t uglifyify src/index.jsx -o public/js/bundle.js
